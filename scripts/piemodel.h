3a21112f4a739694b6f3437527db6d6cc690ada9
Leaf Johnson <leafjohn@gmail.com>
2011-04-09 19:52:54 +0800

    ADD more properties to piemodel

--- scripts/piemodel.h
+++ scripts/piemodel.h
@@ -1,20 +1,21 @@
 #ifndef PIEMODEL_H
 #define PIEMODEL_H
-#include <QObject>
 
+#include <QObject>
 
 class PieModel : public QObject
 {
     Q_OBJECT
 public:
-    Q_PROPERTY (QPixmap icon READ icon WRITE setIcon NOTIFY iconChanged)
     Q_PROPERTY (qreal startAngle READ startAngle WRITE setStartAngle NOTIFY startAngleChanged);
     Q_PROPERTY (qreal endAngle READ endAngle WRITE setStartAngle NOTIFY endAngleChanged);
     Q_PROPERTY (QString name READ name WRITE setStartAngle NOTIFY nameChanged);
     Q_PROPERTY (QString detail READ detail WRITE setStartAngle NOTIFY detailChanged);
+
 public:
     explicit PieModel(QObject *parent = 0);
     virtual ~PieModel();
+
     void setStartAngle(qreal start);
     void setEndAngle(qreal end);
     void setName(const QString &nam);
@@ -24,18 +25,12 @@ public:
     QString name() const;
     QString detail() const;
 
-    void PieModel::setIcon (const QPixmap & icon);
-    const QPixmap & PieModel::icon () const;
-
-
-
 
 signals:
     void startAngleChanged(qreal ang);
     void endAngleChanged(qreal ang);
     void nameChanged(const QString &name);
     void detailChanged(const QString &disc);
-    void iconChanged (const QPixmap & icon);
 
 public slots:
 
@@ -44,7 +39,7 @@ private:
 	qreal _endAngle;
 	QString _name;
 	QString _detail;
-    	QPixmap _icon;
 
 };
+
 #endif // PIEMODEL_H
