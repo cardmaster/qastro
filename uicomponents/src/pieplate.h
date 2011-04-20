791e7255169837f6a1c71639e0f92c6413fed5d9
Leaf Johnson <leafjohn@gmail.com>
2011-04-20 22:40:25 +0800

    Given initial value for radius, change base class of pieplate

--- uicomponents/src/pieplate.h
+++ uicomponents/src/pieplate.h
@@ -1,12 +1,12 @@
 #ifndef PIEPLATE_H
 #define PIEPLATE_H
-#include <QGraphicsObject>
+#include "plateitem.h"
 #include <QPixmap>
 class PieDelegateItem;
 class PieModel;
 
 
-class PiePlate : public QGraphicsObject
+class PiePlate : public PlateItem
 {
     Q_OBJECT
 /*! Properties */
@@ -19,28 +19,23 @@ public:
     void setModel(QList<qreal> model);
     void setModel(QList<QObject *> model);
     void setModel(QList<PieModel *> model);
-    void setRadius(qreal radius);
-    qreal radius() const;
+
     /*! Radius Ratio, the ratio between pie and plate */
     void setRadiusRatio(qreal ratio);
     qreal radiusRatio() const;
     qreal pieRadius() const;
 
-    void setBackground (const QPixmap &background);
-    const QPixmap &background () const;
-
 signals:
     void backgroundChanged (QPixmap background);
 
 protected:
-    QRectF boundingRect() const;
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
 private:
     void updatePieRadius();
     void createPies(int count);
+
 private:
     QList<PieDelegateItem *> _pies;
-    qreal _radius;
     qreal _radiusRatio;
     QPixmap _background;
 
